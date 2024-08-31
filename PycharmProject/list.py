799295fa1df52feb9dffddf40740ebf53de93e29
justinejeraldrosel <anxespero@gmail.com>
2024-08-31 12:47:17 +0800

    Update list.py

--- PycharmProject/list.py
+++ PycharmProject/list.py
@@ -1,2 +1,12 @@
 color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
 print (color)
+
+color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
+print (color[5])
+
+color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
+print (color[-6])
+
+color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
+print (color[3:7])
+
