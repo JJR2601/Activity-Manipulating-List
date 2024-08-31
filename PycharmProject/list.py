fe0f9a2a6bfabc1a7dca0e970358171b94419444
justinejeraldrosel <anxespero@gmail.com>
2024-08-31 12:59:45 +0800

    Update list.py

--- PycharmProject/list.py
+++ PycharmProject/list.py
@@ -1,12 +1,27 @@
-color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
-print (color)
-
-color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
-print (color[5])
-
-color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
-print (color[-6])
-
+# Print list
 color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
 print (color[3:7])
+
+# Change certain item in list
+color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
+color[2:6] = ["magenta", "maroon"]
+print (color)
+
+# add in list
+color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
+
+color.append("magenta")
+color.append("cyan")
+color.append("maroon")
+
+color.extend(["magenta", "cyan", "maroon"])
+
+print(color)
+
+# Delete list
+color = ["blue", "yellow", "red", "green", "gold", "orange", "violet", "brown", "black", "white"]
+del color[5]
+print(color)
+
+
 
